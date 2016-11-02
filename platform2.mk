e3a643cc87ad56dea89fcc3f6b6164867e371359
David <Davidteri91@gmail.com>
2016-11-02 16:45:40 +0100

    shinano: CM'ify
    Change-Id: Iae7f1f1b85b782b42858e416a6282979125e0afd
    Signed-off-by: David Viteri <davidteri91@gmail.com>

---
+++ platform2.mk
@@ -0,0 +1,28 @@
+# Copyright 2016 David Viteri <davidteri91@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SONY_ROOT := device/sony/shinano/rootdir
+
+# ANT+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
+# Sensors config
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/etc/sec_config:system/etc/sec_config
