358bad6b30d39c38e207ff374306e60a1e426b54
David <Davidteri91@gmail.com>
2017-01-02 19:05:01 +0100

    shinano: allow z3 and z3c
    leo and aries have different name to avoid conflicts with others devices
    
    Change-Id: Ib0ffdf99315b9f9c8fa0b735ea48e286dee057d0
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(filter-out aries castor castor_windy leo scorpion scorpion_windy sirius,$(TARGET_DEVICE)),)
+ifeq ($(filter-out aries castor castor_windy leo scorpion scorpion_windy sirius z3 z3c,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
