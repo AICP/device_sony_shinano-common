991ac554d25c1f7666b6af4ae86e33d19e9745ee
SpiritCroc <spiritcroc@gmail.com>
2017-10-31 13:48:11 +0100

    shinano: add BOARD_KERNEL_IMAGE_NAME
    Change-Id: I8e14aff160a0bdc3472f0f923595650a8687e62f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,6 +24,7 @@ PRODUCT_PLATFORM:= shinano
 MALLOC_SVELTE := true
 
 # Kernel information
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
