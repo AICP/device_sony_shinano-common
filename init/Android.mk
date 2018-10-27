41494ccde25848bc845c36ce57dd8de069c1aaa7
nailyk <git@nailyk.fr>
2018-10-27 16:42:12 +0200

    shinano: init: fix compile for P
    Change-Id: I907491704503a27a3ff3aebf25e782488393df4d

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_CFLAGS := -Wall
+LOCAL_CFLAGS := -Wall -std=gnu++1z
 LOCAL_STATIC_LIBRARIES := libbase libselinux
 LOCAL_SRC_FILES := init_shinano.cpp
 LOCAL_MODULE := libinit_shinano
