c23a8df3689108ddf299a588696404820322c11b
SpiritCroc <dev@spiritcroc.de>
2019-12-22 15:06:32 +0100

    [TMP] shinano: Disable sepolicy
    Change-Id: Iacc7e45d7fc02cc5d46b35c2030b8df41f7f3d7f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,7 @@ BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom msm_rtb.filter=0x3b7 ehci-hcd.park=3 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y vmalloc=300M
 BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_CMDLINE  += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_DTB_EXTRA_FLAGS := --force-v2
@@ -84,8 +85,8 @@ TARGET_INIT_VENDOR_LIB := libinit_shinano
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/sony/shinano-common/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/sony/shinano-common/sepolicy
 
 # Props for hotplugging
 TARGET_SYSTEM_PROP += device/sony/shinano-common/system.prop
