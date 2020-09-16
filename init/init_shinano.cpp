3463548deecdc3588a563a312226ff2d84bb38ac
Lucchetto <lucchetto.tie@live.com>
2020-09-16 18:48:36 +0200

    shinano-common: Adapt libinit to Android 11
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>
    Change-Id: Ia6b6f753891b639dad4e380bdba6b594faf2a550
    Test: build
    Test: getprop | grep s1boot
    Signed-off-by: koron393 <koron393@gmail.com>

--- init/init_shinano.cpp
+++ init/init_shinano.cpp
@@ -17,25 +17,48 @@
 
 #include <stdlib.h>
 
-#include "vendor_init.h"
-#include "property_service.h"
-#include "log/log.h"
-#include "util.h"
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 
-#include <sys/system_properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
-static void import_kernel_nv(const std::string& key,
-        const std::string& value, bool for_emulator __attribute__((unused)))
+
+void device_property_override(char const prop[], char const value[], bool add = true)
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void import_kernel_cmdline(const std::function<void(const std::string&, const std::string&)>& fn) {
+    std::string cmdline;
+    android::base::ReadFileToString("/proc/cmdline", &cmdline);
+
+    for (const auto& entry : android::base::Split(android::base::Trim(cmdline), " ")) {
+        std::vector<std::string> pieces = android::base::Split(entry, "=");
+        if (pieces.size() == 2) {
+            fn(pieces[0], pieces[1]);
+        }
+    }
+}
+
+static void import_kernel_nv(const std::string& key, const std::string& value)
 {
     if (key.empty()) return;
 
     // We only want the bootloader version
     if (key == "oemandroidboot.s1boot") {
-		android::init::property_set("ro.boot.oemandroidboot.s1boot", value.c_str());
+        device_property_override("ro.boot.oemandroidboot.s1boot", value.c_str());
     }
 }
 
 void vendor_load_properties()
 {
-    android::init::import_kernel_cmdline(0, import_kernel_nv);
+    import_kernel_cmdline(import_kernel_nv);
 }
